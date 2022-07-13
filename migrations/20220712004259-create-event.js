0f201893a9dffcd8366ee01d7f429684a87d3db6
sammyatparachute <48567572+sammyatparachute@users.noreply.github.com>
2022-07-13 17:24:06 -0400

    Updated with completed day 4 code

---
+++ migrations/20220712004259-create-event.js
@@ -0,0 +1,28 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("events", {
+      event_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      start_time: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      end_time: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("events");
+  },
+};
